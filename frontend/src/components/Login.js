e1be9a9e4fa72a7cfecee3e33873a29f6154fc8d
Chiuweiyu <b07303037@ntu.edu.tw>
2021-11-21 22:11:52 +0800

    Add files via upload

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -13,29 +13,32 @@ function Login() {
   }
 
   const handleLogin = () => {
-    if (emailId.length === 0) {
-      return
-    }
-
-    axios
-      .get(api(`/user/${emailId}`))
-      .then((res) => {
-        if (res.data) {
-          setUser(res.data)
-          setMessage(
-            `Hi! ${
-              res.data.displayName ? res.data.displayName : res.data.emailId
-            }`,
-          )
-        } else {
-          setMessage('Wrong User. Try again or register one.')
-          setEmailId('')
-        }
-      })
-      .catch((err) => {
-        console.log(err)
-        setMessage('Ooops Login fail...')
-      })
+    try{
+      if (emailId.length === 0) {
+        return
+      }
+  
+      axios
+        .get(api(`/user/${emailId}`))
+        .then((res) => {
+          if (res.data) {
+            setUser(res.data)
+            setMessage(
+              `Hi! ${
+                res.data.displayName ? res.data.displayName : res.data.emailId
+              }`,
+            )
+          } else {
+            setMessage('Wrong User. Try again or register one.')
+            setEmailId('')
+          }
+        })
+        .catch((err) => {
+          console.log(err)
+          setMessage('Ooops Login fail...')
+        })
+    }catch(e){};
+    
   }
 
   const handleRegister = () => {
@@ -43,25 +46,27 @@ function Login() {
     // if yes, login, say: you have registered, welcome
     // if not, create one and login, say welcome
     // TODO: should check it's a valid email
-
-    if (emailId.length === 0) {
-      return
-    }
-
-    axios
-      .post(api(`/user/${emailId}/register`))
-      .then((res) => {
-        setUser(res.data)
-        setMessage(
-          `Hi! ${
-            res.data.displayName ? res.data.displayName : res.data.emailId
-          }`,
-        )
-      })
-      .catch((err) => {
-        console.log('errr: ', err)
-        setMessage('Register Error')
-      })
+    try{
+      if (emailId.length === 0) {
+        return
+      }
+  
+      axios
+        .post(api(`/user/${emailId}/register`))
+        .then((res) => {
+          setUser(res.data)
+          setMessage(
+            `Hi! ${
+              res.data.displayName ? res.data.displayName : res.data.emailId
+            }`,
+          )
+        })
+        .catch((err) => {
+          console.log('errr: ', err)
+          setMessage('Register Error')
+        })
+    }catch(e){};
+    
   }
 
   return (
